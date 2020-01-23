f9e61078a49f46d0c3239f6505cccc6f5df7ee50
apple <apple@apples-MacBook-Pro-2.local>
2020-01-23 14:23:42 -0800

    added styled-components

--- src/nasaCard.js
+++ src/nasaCard.js
@@ -3,6 +3,13 @@ import {
   Card, CardImg, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle
 } from 'reactstrap';
+import styled from 'styled-components';
+
+const NasaImg = styled.img`
+  width: 1000px;
+  
+
+`;
 
 const NasaCard = (props) => {
 return (
@@ -11,7 +18,7 @@ return (
           <CardTitle>{props.data.title}</CardTitle>
           <CardSubtitle>Date: {props.data.date}</CardSubtitle>
         </CardBody>
-        <img width="100%" src={props.data.hdurl} alt={props.data.title} />
+        <NasaImg width="100%" src={props.data.hdurl} alt={props.data.title} />
         <CardBody>
           <CardText>Description: {props.data.explanation}</CardText>
           <CardSubtitle>Photo Copyright by: {props.data.copyright}</CardSubtitle>
